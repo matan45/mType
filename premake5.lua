cf26dd2d0003328dad5cab0e612dd9f907a9b60a
matan migdal <matan33214@gmail.com>
2025-08-11 09:37:29 +0300

    add gitignore

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+workspace "Interpreter"
+   configurations { "Debug", "Release" }
+   platforms { "x64" }
+   location "."  -- Place generated solution files in the root directory
+   startproject "mType"
+   
+   
+-- Set a consistent location for build artifacts
+builddir = "bin/%{prj.name}/%{cfg.buildcfg}/%{cfg.platform}"
+
+-- Project 1: mType
+project "mType"
+   kind "ConsoleApp"
+   language "C++"
+   cppdialect "C++20"
+
+   -- Specify project files and their directory
+   location "mType"
+   files { "mType/**.hpp", "mType/**.cpp" }
+   
+    -- Set the target output directory using the shared variable
+   targetdir (builddir)
+
+   defines { "_CRT_SECURE_NO_WARNINGS" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
+
+
+
+-- Project: assimp and softal need to build with cmake...
