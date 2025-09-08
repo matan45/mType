5431787ae284cb9bcab0c034b8ab010363a3296f
matan45 <matan33214@gmail.com>
2025-09-08 17:46:33 +0300

    remove tests

--- mType/tests/suites/CollectionsTestSuite.cpp
+++ mType/tests/suites/CollectionsTestSuite.cpp
@@ -11,54 +11,16 @@ namespace tests::testSuite
                         passPath + "arrayBasic.mt");
         addOutputVerificationTest("Array Literal Syntax",
                         passPath + "arrayLiteral.mt");
-        addOutputVerificationTest("Array Operations",
-                        passPath + "arrayOperations.mt");
-        addOutputVerificationTest("Array Type Safety",
-                        passPath + "arrayTypeSafety.mt");
-
-        // List tests
-        addOutputVerificationTest("Basic List Creation",
-                        passPath + "listBasic.mt");
-        addOutputVerificationTest("List Operations",
-                        passPath + "listOperations.mt");
 
         // Map tests
         addOutputVerificationTest("Basic Map Creation",
                         passPath + "mapBasic.mt");
         addOutputVerificationTest("Map Literal Syntax",
                         passPath + "mapLiteral.mt");
-        addOutputVerificationTest("Map Operations",
-                        passPath + "mapOperations.mt");
-
-        // Set tests
-        addOutputVerificationTest("Basic Set Creation",
-                        passPath + "setBasic.mt");
-        addOutputVerificationTest("Set Operations",
-                        passPath + "setOperations.mt");
-
-        // Queue tests
-        addOutputVerificationTest("Basic Queue Creation",
-                        passPath + "queueBasic.mt");
-        addOutputVerificationTest("Queue Operations",
-                        passPath + "queueOperations.mt");
-
-        // Stack tests
-        addOutputVerificationTest("Basic Stack Creation",
-                        passPath + "stackBasic.mt");
-        addOutputVerificationTest("Stack Operations",
-                        passPath + "stackOperations.mt");
 
         // For-each loop tests
         addOutputVerificationTest("For-Each with Array",
                         passPath + "forEachArray.mt");
-        addOutputVerificationTest("For-Each with List",
-                        passPath + "forEachList.mt");
-        addOutputVerificationTest("For-Each with Map",
-                        passPath + "forEachMap.mt");
-        addOutputVerificationTest("For-Each with Set",
-                        passPath + "forEachSet.mt");
-        addOutputVerificationTest("For-Each Control Flow",
-                        passPath + "forEachControlFlow.mt");
 
         // Integration tests
         addOutputVerificationTest("Collections as Parameters",
@@ -69,24 +31,13 @@ namespace tests::testSuite
                         passPath + "nestedCollections.mt");
         addOutputVerificationTest("Comprehensive Nested For-Each",
                         passPath + "comprehensiveNestedForEach.mt");
-        addOutputVerificationTest("Collections in Classes",
-                        passPath + "collectionsInClasses.mt");
 
         // Error tests (expected to fail)
         addTestFromFile("Array Type Mismatch Error",
                         errorPath + "arrayTypeMismatch.mt",
                         TestType::ERROR_EXPECTED);
-        addTestFromFile("Map Key Type Error",
-                        errorPath + "mapKeyTypeError.mt",
-                        TestType::ERROR_EXPECTED);
-        addTestFromFile("Invalid Index Access Error",
-                        errorPath + "invalidIndexAccess.mt",
-                        TestType::ERROR_EXPECTED);
         addTestFromFile("For-Each Type Mismatch Error",
                         errorPath + "forEachTypeMismatch.mt",
-                        TestType::ERROR_EXPECTED);
-        addTestFromFile("Empty Collection Access Error",
-                        errorPath + "emptyCollectionAccess.mt",
                         TestType::ERROR_EXPECTED);
     }
 }
