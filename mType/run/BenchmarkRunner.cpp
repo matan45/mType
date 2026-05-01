779c99db09a4efeaefc7cd03acda18c66cee56a9
matan <matan33214@gmail.com>
2026-05-01 11:08:02 +0300

    fix

--- mType/run/BenchmarkRunner.cpp
+++ mType/run/BenchmarkRunner.cpp
@@ -31,7 +31,7 @@ namespace runMain
 {
 namespace
 {
-    constexpr std::array<const char*, 35> CANONICAL_SCRIPTS = {
+    constexpr std::array<const char*, 36> CANONICAL_SCRIPTS = {
         "arithmetic_tight_loop.mt",
         "method_dispatch.mt",
         "object_alloc.mt",
