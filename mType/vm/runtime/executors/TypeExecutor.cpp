f25888e0af9b0de0932c2fe9fb20e31d8fcbdb60
matan <matan33214@gmail.com>
2026-05-04 17:59:01 +0300

    update

--- mType/vm/runtime/executors/TypeExecutor.cpp
+++ mType/vm/runtime/executors/TypeExecutor.cpp
@@ -663,7 +663,7 @@ namespace vm::runtime
         }
         else if (value::isString(val)) {
             try {
-                return std::stoll(value::asString(val));
+                return static_cast<int64_t>(std::stoll(value::asString(val)));
             } catch (...) {
                 throwCastError("Cannot cast string to int: " + value::asString(val));
             }
@@ -671,7 +671,7 @@ namespace vm::runtime
         else if (value::isInternedString(val)) {
             const std::string& str = value::asInternedString(val).getString();
             try {
-                return std::stoll(str);
+                return static_cast<int64_t>(std::stoll(str));
             } catch (...) {
                 throwCastError("Cannot cast string to int: " + str);
             }
